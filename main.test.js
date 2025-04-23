e441cd4b2e237aace948ed417eec4528bd3f64b4
lordreignera <lordreignera@gmail.com>
2025-04-23 09:43:17 +0300

    input a folder of .github to run actions

--- main.test.js
+++ main.test.js
@@ -1,4 +1,5 @@
-const add = require ('./main.js');
-test('adds 1 + 2 to equal 3'), () => {
+const add = require('./main.js');
+
+test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
-    };
+});
